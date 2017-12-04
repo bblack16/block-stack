2ccdbc5ea108d769e275a11eb5cd0e1a3d43e3a4
Brandon Black <d2sm10@hotmail.com>
2017-12-04 08:12:23 -0700

    Added new default locals after refactoring view.

--- lib/block_stack/ui/blocks/form.rb
+++ lib/block_stack/ui/blocks/form.rb
@@ -3,7 +3,14 @@ module BlockStack
 
     def default_locals
       {
-        model: nil
+        model:         nil,
+        form:          nil,
+        name:          'form',
+        save_text:     nil,
+        save_to:       '/dformed',
+        method:        :post,
+        save_redirect: nil,
+        df_class:      nil
       }
     end
 
