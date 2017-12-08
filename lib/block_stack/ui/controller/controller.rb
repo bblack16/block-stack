d49f50367c6b0c28ca24fe59dff6cccb70bd5f98
Brandon Black <d2sm10@hotmail.com>
2017-12-08 08:08:35 -0700

    Changed crud route to use route templates.

--- lib/block_stack/ui/controller/controller.rb
+++ lib/block_stack/ui/controller/controller.rb
@@ -6,73 +6,27 @@ module BlockStack
       default_view_folder: 'default' # Sets the default folder to load fallback views from.
     )
 
-    class << self
-      alias crud_api crud
-    end
-
     def self.menu
       base_server.menu
     end
 
     def self.crud(opts = {})
-      crud_api(opts)
-      engine = opts[:engine] || settings.default_renderer
+      self.model = opts[:model] if opts[:model]
+      self.prefix = opts.include?(:prefix) ? opts[:prefix] : model.plural_name
+
       add_sub_menus(
         {
           title: model.clean_name.pluralize,
-          fa_icon: fa_icon,
+          fa_icon: respond_to?(:fa_icon) ? fa_icon : nil ,
           items: [
-            { title: 'Browse', fa_icon: 'eye', attributes: { href: "/#{prefix}/" } },
-            { title: 'New Game', fa_icon: 'plus', attributes: { href: "/#{prefix}/new" } }
+            { title: 'Browse', fa_icon: 'list', attributes: { href: "/#{prefix}/" } },
+            { title: "New #{model.clean_name}", fa_icon: 'plus', attributes: { href: "/#{prefix}/new" } }
           ]
         }
       )
 
-      get '/' do
-        begin
-          @models = model.all
-          send(engine, :"#{model.plural_name}/index")
-        rescue Errno::ENOENT => e
-          @model = model
-          @models = model.all
-          slim :"#{settings.default_view_folder}/index"
-        end
-      end
-
-      get '/new' do
-        begin
-          @model = model.new(opts[:new_defaults] || {})
-          send(engine, :"#{model.plural_name}/new")
-        rescue Errno::ENOENT => e
-          slim :"#{settings.default_view_folder}/new"
-        end
-      end
-
-      get '/:id' do
-        begin
-          @model = model.find(params[:id])
-          if @model
-            send(engine, :"#{model.plural_name}/show")
-          else
-            redirect "/#{model.plural_name}", notice: "Could not locate any #{model.clean_name.pluralize} with an id of #{params[:id]}."
-          end
-        rescue Errno::ENOENT => e
-          slim :"#{settings.default_view_folder}/show"
-        end
-      end
-
-      get '/:id/edit' do
-        begin
-          @model = model.find(params[:id])
-          if @model
-            send(engine, :"#{model.plural_name}/edit")
-          else
-            redirect "/#{model.plural_name}", notice: "Could not locate any #{model.clean_name.pluralize} with an id of #{params[:id]}."
-          end
-        rescue Errno::ENOENT => e
-          slim :"#{settings.default_view_folder}/edit"
-        end
-      end
+      attach_route_template_group(:crud, *(opts[:ignore] || []))
+      true
     end
 
   end
