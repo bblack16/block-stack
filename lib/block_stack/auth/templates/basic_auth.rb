f96494381bb8f719c8d55e0ce41b3a05e98d74b9
Brandon Black <d2sm10@hotmail.com>
2017-12-11 07:15:47 -0700

    Created basic auth template.

--- lib/block_stack/auth/templates/basic_auth.rb
+++ lib/block_stack/auth/templates/basic_auth.rb
@@ -1,1 +1,11 @@
-# TODO Everything
+
+module BlockStack
+
+  add_template(:basic_auth, :block_stack_auth) do |server, opts|
+    server.set(:unauthorized_message, (opts[:unauthorized_message].to_s || 'Not Authorized!') + "\n")
+    server.helpers(BlockStack::Helpers::BasicAuth)
+    server.add_auth_sources(BlockStack::Authentication::Basic.new)
+    # server.add_auth_providers(BlockStack::)
+  end
+
+end
