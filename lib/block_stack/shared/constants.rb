6559c6d953e29fbda3f75e05203a660d0b34e7d9
Brandon Black <d2sm10@hotmail.com>
2017-12-11 07:18:19 -0700

    Added shortcuts for BBLib::HTML and BBLib::HTML::Tag.

--- lib/block_stack/shared/constants.rb
+++ lib/block_stack/shared/constants.rb
@@ -1,4 +1,6 @@
 module BlockStack
   VERSION = '1.0.0'.freeze
-  VERBS = [:get, :post, :put, :delete, :patch, :head, :options, :link, :unlink].freeze
+  VERBS   = [:get, :post, :put, :delete, :patch, :head, :options, :link, :unlink].freeze
+  HTML    = BBLib::HTML
+  Tag     = BBLib::HTML::Tag
 end
