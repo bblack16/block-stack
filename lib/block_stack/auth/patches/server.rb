9868853c8663b736762cee0a5e629aa2dd5c2f4d
Brandon Black <d2sm10@hotmail.com>
2017-12-20 07:19:31 -0700

    Refactored a lot of how auth and auth works.

--- lib/block_stack/auth/patches/server.rb
+++ lib/block_stack/auth/patches/server.rb
@@ -2,21 +2,21 @@ module BlockStack
   class Server
     enable :sessions
 
-    attr_ary_of Authentication::Source, :auth_sources, default: [], singleton: true, add_rem: true
-    attr_ary_of Authentication::Provider, :auth_providers, default: [], singleton: true, add_rem: true
+    attr_ary_of Authentication::Source, :auth_sources, default: [], singleton: true, add_rem: true, adder_name: 'add_auth_source', remover_name: 'remove_auth_source'
+    attr_ary_of Authentication::Provider, :auth_providers, default: [], singleton: true, add_rem: true, adder_name: 'add_auth_provider', remover_name: 'remove_auth_provider'
     attr_ary_of Authorization::Route, :authorizations, default: [], singleton: true, add_rem: true
-    attr_ary_of [String, Regexp], :skip_auth_routes, default: [/^\/(assets\/)?(stylesheets|javascript|fonts)\//i, maps_prefix, /^\/__OPAL_SOURCE_MAPS__/i], singleton: true
+    attr_ary_of [String, Regexp], :skip_auth_routes, default: [/^\/(assets\/)?(stylesheets|javascript|fonts)\//i, config.maps_prefix, /^\/__OPAL_SOURCE_MAPS__/i], singleton: true
 
     bridge_method :authorizations, :auth_providers, :auth_sources, :skip_auth_routes
 
-    set(
+    config(
       authorization:                true,  # When true authorization rules are run for each request
       authentication:               true,  # When true authentication rules are run for each request
       deny_by_default:              false, # When true, if noth authorization rules match a request, the request is denied by default.
       authentication_failure_route: nil,   # The route to redirect to when authentication fails. Setting to nil ignores a redirect and returns a 403.
       authorization_failure_route:  nil,
       homepage:                     '/',   # Sets the url that is considered to be the home page. Mostly used in redirects.
-      user_model:                   BlockStack::User # Sets the model that is used when registering new users
+      login_model:                   BlockStack::Authentication::Login # Sets the model that is used when registering new users
     )
 
     def self.authorize(action, object, allow, opts = {})
@@ -35,7 +35,7 @@ module BlockStack
         next unless creds
         auth_providers.each do |provider|
           next if current_user
-          user = provider.authenticate(*(creds + [request, params]))
+          user = provider.authenticate(*[creds].flatten(1), request: request, params: params)
           next unless user
           begin
             user.current_login = Time.now
@@ -57,7 +57,7 @@ module BlockStack
       matches = authorizations.find_all do |authorization|
         authorization.match?(request.request_method.downcase, request.path)
       end
-      return !settings.deny_by_default if matches.empty?
+      return !config.deny_by_default if matches.empty?
       matches.any? { |auth| auth.permit?(current_user, request, params) }
     end
 
@@ -78,9 +78,11 @@ module BlockStack
       end
     end
 
-    def current_user
+    def current_login
       session[:user]
     end
+
+    alias_method :current_user, :current_login
 
     def logout
       current_user.last_login = current_user.current_login
@@ -93,8 +95,8 @@ module BlockStack
     def unauthorized!
       logger.info("Authorization FORBIDDEN for #{current_user.name} for #{request.path_info}")
       return custom_unauthorized! if respond_to?(:custom_unauthorized!)
-      if settings.authorization_failure_route
-        redirect settings.authorization_failure_route, 303, notice: 'You are not authorized for that!', severity: :error
+      if config.authorization_failure_route
+        redirect config.authorization_failure_route, 303, notice: 'You are not authorized for that!', severity: :error
       else
         halt 403, 'You are not authorized for that.'
       end
@@ -103,19 +105,18 @@ module BlockStack
     # TODO Enhance logging to log whether auth was not provided or was but failed
     def unauthenticated!
       logger.info("Authentication not provided or failed for request #{request.object_id}.")
-      p "ORIGINAL UNAUTH: #{self.respond_to?(:custom_unauthenticated!)}"
-      p self
       return custom_unauthenticated! if respond_to?(:custom_unauthenticated!)
-      if settings.authentication_failure_route
-        redirect to(settings.authentication_failure_route), 303, notice: 'Please provide a valid login.', severity: :error
+      if config.authentication_failure_route
+        redirect to(config.authentication_failure_route), 303, notice: 'Please provide a valid login.', severity: :error
       else
         halt 403, 'Not authorized. Please provide valid credentials.'
       end
     end
 
     def protected!
+      return if skip_auth?
       if authenticate!
-        if settings.authorization
+        if config.authorization
           if authorize!
             logger.debug("Authorization ALLOWED for #{current_user.name} for #{request.path_info}.")
           else
@@ -128,10 +129,10 @@ module BlockStack
     end
 
     # TODO Move to "protect_all" template
-    before do
-      unless skip_auth? || !settings.authentication
-        protected!
-      end
-    end
+    # before do
+    #   unless skip_auth? || !config.authentication
+    #     protected!
+    #   end
+    # end
   end
 end
