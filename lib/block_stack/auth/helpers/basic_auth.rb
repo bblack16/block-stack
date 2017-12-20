930c8d691e6c04d828d08f182debd409ecedcaa6
Brandon Black <d2sm10@hotmail.com>
2017-12-20 07:19:04 -0700

    Removed debug line.

--- lib/block_stack/auth/helpers/basic_auth.rb
+++ lib/block_stack/auth/helpers/basic_auth.rb
@@ -2,7 +2,6 @@ module BlockStack
   module Helpers
     module BasicAuth
       def custom_unauthenticated!
-        p "CUSTOM UNAUTHENTICATED"
         headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
         halt 401, "Not authorized\n"
       end
