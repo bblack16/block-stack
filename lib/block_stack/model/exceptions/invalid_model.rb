a81bc85648a44f172c0c077600cfb3e238f7ef4f
bj002440 <bblack@digitalglobe.com>
2018-01-18 19:38:35 -0700

    Changed location of invalid model class.

--- lib/block_stack/model/exceptions/invalid_model.rb
+++ lib/block_stack/model/exceptions/invalid_model.rb
@@ -1,19 +1,17 @@
 module BlockStack
-  module Model
-    class InvalidModel < StandardError
-      attr_reader :errors
+  class InvalidModel < StandardError
+    attr_reader :errors
 
-      def initialize(model_or_msg = 'Invalid model', model = nil)
-        case model_or_msg
-        when BlockStack::Model
-          @errors = model_or_msg.errors
-          super("#{model_or_msg.clean_name} #{model_or_msg.id ? "#{model_or_msg.id} " : nil}could not be saved because the following fields were invalid: #{model_or_msg.errors.keys.join_terms}.")
-        else
-          @errors = model ? model.errors : {}
-          super(model_or_msg.to_s)
-        end
+    def initialize(model_or_msg = 'Invalid model', model = nil)
+      case model_or_msg
+      when BlockStack::Model
+        @errors = model_or_msg.errors
+        super("#{model_or_msg.clean_name} #{model_or_msg.id ? "#{model_or_msg.id} " : nil}could not be saved because the following fields were invalid: #{model_or_msg.errors.keys.join_terms}.")
+      else
+        @errors = model ? model.errors : {}
+        super(model_or_msg.to_s)
       end
-
     end
+
   end
 end
