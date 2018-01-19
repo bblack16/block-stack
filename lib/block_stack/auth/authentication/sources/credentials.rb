0e2f6d457727e01fea5210cdfd82fa169b8da1cd
bj002440 <bblack@digitalglobe.com>
2018-01-19 15:42:11 -0700

    Attempted to fix html encoding issues and added todos.

--- lib/block_stack/auth/authentication/sources/credentials.rb
+++ lib/block_stack/auth/authentication/sources/credentials.rb
@@ -1,12 +1,13 @@
 module BlockStack
   module Authentication
-    class Credentials < Source
+    class Credentials < Source # TODO Change to HTML Form
       attr_sym :user_param, default: :user
       attr_sym :pass_param, default: :password
 
       def credentials(request, params)
+        # TODO Fix encoding issues from HTML forms
         hash = request.body.read.split('&').hmap do |param|
-          param.split('=', 2)
+          CGI.unescapeHTML(param).split('=', 2)
         end.keys_to_sym
         user = hash[user_param]
         pass = hash[pass_param]
@@ -14,6 +15,12 @@ module BlockStack
         [user, pass]
       rescue => e
         false
+      end
+
+      protected
+
+      def simple_setup
+        require 'cgi' unless defined?(CGI)
       end
 
     end
