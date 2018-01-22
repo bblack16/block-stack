85431284d46e55e1573b9220f1c4b3519dd25e65
Brandon Black <d2sm10@hotmail.com>
2018-01-22 08:38:25 -0700

    Moved controller base setting to a more sensible location.

--- lib/block_stack.rb
+++ lib/block_stack.rb
@@ -4,6 +4,3 @@ require_relative 'block_stack/shared'
 require_relative 'block_stack/model'
 require_relative 'block_stack/server'
 require_relative 'block_stack/ui'
-
-# TODO Decide if the default should load all controllers (probably not)
-BlockStack::Server.set(controller_base: BlockStack::Controller)
