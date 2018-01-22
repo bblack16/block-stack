f47a3584118dda58c3dab55fa9f1d22a24f57d4d
Brandon Black <d2sm10@hotmail.com>
2018-01-22 08:39:05 -0700

    Refactored how the param source works.

--- lib/block_stack/auth/authentication/sources/param.rb
+++ lib/block_stack/auth/authentication/sources/param.rb
@@ -1,13 +1,16 @@
 module BlockStack
   module Authentication
     class Param < Source
-      attr_sym :param, default: :api_key
+      attr_ary_of [String], :params, default: ['user', 'password'], pre_proc: proc { |x| [x].flatten.map(&:to_s) }
 
       def credentials(request, params)
-        return false unless params.include?(param) && !params[param].empty?
-        params[param]
+        self.params.map do |param|
+          return false unless params.include?(param)
+          params[param]
+        end
+      rescue => _e
+        false
       end
-
     end
   end
 end
