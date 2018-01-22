c13a2d44ac3f0009cd738f00a65df01311a4b05c
Brandon Black <d2sm10@hotmail.com>
2018-01-22 08:39:32 -0700

    Changed session to login rather than user.

--- lib/block_stack/auth/helpers/cas.rb
+++ lib/block_stack/auth/helpers/cas.rb
@@ -3,7 +3,7 @@ module BlockStack
     module CAS
       def custom_unauthenticated!
         halt 401, 'You must authenticate first.' unless session['cas'] && session['cas']['user']
-        session[:user] = config.cas_login_class.new(name: session['cas']['user'], extra_attributes: session['cas']['extra_attributes']) unless session[:user]
+        session[:login] = config.cas_login_class.new(name: session['cas']['user'], extra_attributes: session['cas']['extra_attributes']) unless session[:login]
       end
     end
   end
