2a52a88efc3619628a2e252bac0d3e8b8083a7dd
Brandon Black <d2sm10@hotmail.com>
2018-02-26 07:40:49 -0700

    Added scoping to loaders via a jquery selector.

--- lib/block_stack/ui/app/javascript/block_stack/loaders.rb
+++ lib/block_stack/ui/app/javascript/block_stack/loaders.rb
@@ -1,41 +1,41 @@
 # Various methods to load or reload elements based on the DOM
 module Loaders
 
-  def self.load_all
+  def self.load_all(selector = 'body')
     # Main Menu slider
-    Loaders.menu_slider
+    Loaders.menu_slider(selector)
 
-    Loaders.dformed
+    Loaders.dformed(selector)
 
     # Load delete links
-    Loaders.delete_model_buttons
+    Loaders.delete_model_buttons(selector)
 
     # Initialize tooltips and data tables
-    Loaders.tooltips
-    Loaders.data_tables
+    Loaders.tooltips(selector)
+    Loaders.data_tables(selector)
 
     # Initialize AnimateOnScroll scripts
-    # Loaders.animate_on_scroll
+    # Loaders.animate_on_scroll(selector)
 
     # Patch for alertify
-    Loaders.alertify
+    Loaders.alertify(selector)
 
-    Loaders.date_pickers
-    Loaders.toggle
-    Loaders.select_2
-    Loaders.ripple
-    Loaders.floating_labels
-    Loaders.autosize_textareas
-    Loaders.baguette_box_galleries
+    Loaders.date_pickers(selector)
+    Loaders.toggle(selector)
+    Loaders.select_2(selector)
+    Loaders.ripple(selector)
+    Loaders.floating_labels(selector)
+    Loaders.autosize_textareas(selector)
+    Loaders.baguette_box_galleries(selector)
 
     # Fires off any notices on the page
-    Loaders.notices
+    Loaders.notices(selector)
 
-    # Loaders.lazy_load
+    Loaders.lazy_load(selector)
   end
 
-  def self.menu_slider
-    Element['#menu-toggle'].on :click do |event|
+  def self.menu_slider(selector = 'body')
+    Element[selector].find('#menu-toggle').on :click do |event|
       menu = Element['#menu']
       if menu.has_class?(:hide)
         menu.remove_class(:hide)
@@ -49,8 +49,8 @@ module Loaders
     end
   end
 
-  def self.delete_model_buttons
-    Element['.delete-model-btn'].each do |elem|
+  def self.delete_model_buttons(selector = 'body')
+    Element[selector].find('.delete-model-btn').each do |elem|
       elem.on :click do |evt|
         url = elem.attr(:'del-url')
         evt.prevent_default
@@ -71,12 +71,12 @@ module Loaders
     end
   end
 
-  def self.tooltips
-    Element['[tooltip="true"],[data-toggle="tooltip"],[title]'].JS.tooltip
+  def self.tooltips(selector = 'body')
+    Element[selector].find('[tooltip="true"],[data-toggle="tooltip"],[title]').JS.tooltip
   end
 
-  def self.data_tables
-    Element['.data-table,.data_table'].each do |table|
+  def self.data_tables(selector = 'body')
+    Element[selector].find('.data-table,.data_table').each do |table|
       opts = { buttons: [:copy, :excel, :colvis, :print] }
       opts[:ajax] = table.attr('dt_ajax') if table.attr('dt_ajax')
       opts['oLanguage'] = {
@@ -93,22 +93,22 @@ module Loaders
   #   `AOS.init();`
   # end
 
-  def self.alertify
+  def self.alertify(selector = 'body')
     `alertify.parent(document.body)`
   end
 
   # TODO Add calendar icon to inputs
-  def self.date_pickers
-    Element['.date-time-picker'].JS.flatpickr({ enableSeconds: true, enableTime: true, altInput: true }.to_n)
-    Element['.date-picker'].JS.flatpickr({ altInput: true }.to_n)
-    Element['.time-picker'].JS.flatpickr({ enableSeconds: true, enableTime: true, noCalendar: true}.to_n)
+  def self.date_pickers(selector = 'body')
+    Element[selector].find('.date-time-picker').JS.flatpickr({ enableSeconds: true, enableTime: true, altInput: true }.to_n)
+    Element[selector].find('.date-picker').JS.flatpickr({ altInput: true }.to_n)
+    Element[selector].find('.time-picker').JS.flatpickr({ enableSeconds: true, enableTime: true, noCalendar: true}.to_n)
     # Element['input.date-time-picker'].each do |elem|
     #   elem.parent.append('<i class="far fa-calendar"></i>') if elem.parent.find('i.far.fa-calendar').empty?
     # end
   end
 
-  def self.toggle
-    Element['input.toggle'].each do |elem|
+  def self.toggle(selector = 'body')
+    Element[selector].find('input.toggle').each do |elem|
       parent = elem.parent
       elem.detach
       label = Element['<label class="switch"/>']
@@ -118,15 +118,15 @@ module Loaders
     end
   end
 
-  def self.select_2
-    Element['.select-2'].JS.select2({
+  def self.select_2(selector = 'body')
+    Element[selector].find('.select-2').JS.select2({
 			theme: 'bootstrap',
       allowClear: true
     }.to_n)
   end
 
-  def self.ripple
-    Element['.ripple'].on :click do |evt|
+  def self.ripple(selector = 'body')
+    Element[selector].find('.ripple').on :click do |evt|
       evt.prevent_default
       offset = evt.current_target.offset
       xpos = evt.page_x - offset.left
@@ -149,30 +149,38 @@ module Loaders
     end
   end
 
-  def self.floating_labels
-    Element['.floating-label'].find('input,textarea,select').on('focusout click change') do |evt|
+  def self.floating_labels(selector = 'body')
+    Element[selector].find('.floating-label').find('input,textarea,select').on('focusout click change') do |evt|
       if evt.target.value.empty?
         evt.target.remove_class('has-value')
       else
         evt.target.add_class('has-value')
       end
     end
-    Element['.floating-label'].find('input,textarea,select').trigger(:focusout)
+    Element[selector].find('.floating-label').find('input,textarea,select').trigger(:focusout)
   end
 
-  def self.autosize_textareas
-    Element['textarea.autosize'].on :input do |evt|
-      clone = evt.target.clone
-      clone.css(:height, 'auto')
-      Element['body'].append(clone)
-      evt.target.css(:height, `#{clone}[0].scrollHeight`.to_s + 'px')
-      clone.remove
+  def self.autosize_textareas(selector = 'body')
+    Element[selector].find('textarea.autosize').each do |elem|
+      elem.on :input do |evt|
+        min_height = elem.attr('data-min-resize-height') || 0
+        max_height = elem.attr('data-max-resize-height') || 400
+        clone = evt.target.clone
+        clone.css(:height, 'auto')
+        clone.css(:width, "#{evt.target.width}px")
+        Element['body'].append(clone)
+        height = `#{clone}[0].scrollHeight`.to_i + 50
+        height = min_height if height < min_height
+        height = max_height if height > max_height
+        evt.target.css(:height, "#{height}px")
+        clone.remove
+      end
     end
-    Element['textarea.autosize'].trigger(:input)
+    Element[selector].find('textarea.autosize').trigger(:input)
   end
 
-  def self.baguette_box_galleries
-    Element["img.baguette-image"].each do |elem|
+  def self.baguette_box_galleries(selector = 'body')
+    Element[selector].find("img.baguette-image").each do |elem|
       parent = elem.parent
       elem.detach
       gallery = Element["<div class='gallery'/>"]
@@ -184,17 +192,17 @@ module Loaders
     `baguetteBox.run('.gallery');`
   end
 
-  def self.dformed
+  def self.dformed(selector = 'body')
     BlockStack.load_forms
   end
 
-  def self.notices
-    Element['#notice'].each do |elem|
+  def self.notices(selector = 'body')
+    Element[selector].find('#notice').each do |elem|
       Alert.log(elem.html, type: elem.attr('notice-severity'), delay: 6000, position: 'top right') if elem.text.strip != ''
     end
   end
 
-  def self.lazy_load
-    `lazyload();`
+  def self.lazy_load(selector = 'body')
+    @lazy_load ||= `new LazyLoad()`
   end
 end
