1285779e7b8821bff522711afa4713dc34e1de31
bj002440 <bblack@digitalglobe.com>
2018-03-02 19:39:10 -0700

    Patched to work with older and new Opals.

--- lib/block_stack/ui.rb
+++ lib/block_stack/ui.rb
@@ -1,9 +1,17 @@
 require_relative 'shared'
 
 require 'opal'
-require 'opal-browser'
-require 'opal-sprockets'
+
+# Check opal version. If the version is 11 or greater we need different opal-sprockets.
+if Gem::Version.new(Opal::VERSION) < Gem::Version.new('0.11.0')
+  OPAL_LEGACY = true
+else
+  OPAL_LEGACY = false
+  require 'opal-sprockets'
+end
+
 require 'opal-jquery'
+require 'opal-browser'
 require 'sass'
 require 'slim'
 require 'dformed' unless defined?(DFormed::VERSION)
