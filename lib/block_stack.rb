5bc8dd798322549b7fc6bcea28a0ec2652c8e9b6
Brandon Black <d2sm10@hotmail.com>
2018-12-13 11:46:33 -0700

    Moved version

--- lib/block_stack.rb
+++ lib/block_stack.rb
@@ -1,5 +1,6 @@
 require 'bblib' unless defined?(BBLib::VERSION)
 
+require_relative 'block_stack/version'
 require_relative 'block_stack/shared'
 require_relative 'block_stack/model'
 require_relative 'block_stack/server'
